a79dca97b3c83c1cf10394d5cbefc40affe61020
Guilherme Ricci <guilhermeericci@gmail.com>
2023-02-23 20:40:10 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const bedrock = require("minecraft-bedrock-server");
+/*const bedrock = require("minecraft-bedrock-server");
 
 const onStart = (data) => console.log("Server started!", data);
 
@@ -6,4 +6,10 @@ bedrock.startServer("1.18.0", onStart, {
   "server-port": 19132,
   "online-mode": true,
   //path: "./bds",
-});
+});*/
+
+const { join } = require("path");
+
+const path = join(__dirname, "/bds");
+
+console.log({ path });
