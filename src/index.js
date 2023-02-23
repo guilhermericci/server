bb3f2da745a1dd5a20c5bf841fbd2a589a8878b2
Guilherme Ricci <guilhermeericci@gmail.com>
2023-02-23 20:35:34 -0300

    Comment path prop

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const bedrock = require("minecraft-bedrock-server");
 
-const onStart = () => console.log("Server started!");
+const onStart = (data) => console.log("Server started!", data);
 
 bedrock.startServer("1.18.0", onStart, {
   "server-port": 19132,
   "online-mode": true,
-  path: "./bds",
+  //path: "./bds",
 });
