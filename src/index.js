b74dbc9db6ce58cc7a68e0b5879ff6f328994bb8
Guilherme Ricci <guilhermeericci@gmail.com>
2023-02-23 21:01:25 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-/*const bedrock = require("minecraft-bedrock-server");
+const bedrock = require("minecraft-bedrock-server");
 
 const onStart = (data) => console.log("Server started!", data);
 
 bedrock.startServer("1.18.0", onStart, {
   "server-port": 19132,
   "online-mode": true,
-  //path: "./bds",
-});*/
+  path: "../bds",
+});
 
+/*
 const { readdirSync } = require("fs");
 
 const path = ".";
 const files = readdirSync(path);
 
 console.log({ files });
+"/
+"
